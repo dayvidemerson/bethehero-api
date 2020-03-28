8626e709d57af22d3948600d7add279a5ff976cf
Dayvid Emerson <dayvidemerson96@gmail.com>
2020-03-27 23:55:37 -0300

    Adicionando teste de integração de unidade de exemplos

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -39,7 +39,7 @@ module.exports = {
       .where('id', id)
       .select('ong_id');
   
-    if (incident.ong_id != ong_id) {
+    if (incident.ong_id !== ong_id) {
       return response.status(401).json({ error: 'Operation not permitted.'});
     }
 
